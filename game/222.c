9d1b553b3b900d240b6a701eec7f4adca164a16c
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-07-16 17:53:52 +0300

    fix(adventure): restore retail end-event state
    (cherry picked from commit bd0d217baa861e19e9c4b1072a8b76ce9c46e9c1)

--- game/222.c
+++ game/222.c
@@ -459,15 +459,6 @@ void AA_EndEvent_DrawMenu(void)
 
 	RECTMENU_ClearInput();
 
-	sdata->Loading.OnBegin.AddBitsConfig0 |= ADVENTURE_ARENA;
-	sdata->Loading.OnBegin.RemBitsConfig0 |= ADVENTURE_BOSS;
-	sdata->Loading.OnBegin.RemBitsConfig8 |= TOKEN_RACE;
-
-	if (IS_BOSS_RACE(gGT->gameMode1))
-	{
-		sdata->Loading.OnBegin.AddBitsConfig8 |= SPAWN_AT_BOSS;
-	}
-
 	if (!didWin)
 	{
 		RECTMENU_Show(&data.menuRetryExit);
@@ -479,12 +470,16 @@ void AA_EndEvent_DrawMenu(void)
 
 	sdata->framesSinceRaceEnded = 0;
 	sdata->numIconsEOR = 1;
+	sdata->Loading.OnBegin.AddBitsConfig0 |= ADVENTURE_ARENA;
 
 	// Load the levelID for Adventure Hub that you came from
 	s16 levSpawn = gGT->prevLEV;
 
 	if (IS_BOSS_RACE(gGT->gameMode1))
 	{
+		sdata->Loading.OnBegin.AddBitsConfig8 |= SPAWN_AT_BOSS;
+		sdata->Loading.OnBegin.RemBitsConfig0 |= ADVENTURE_BOSS;
+
 		// Reward bit of key unlocked, and boss beaten.
 		rewardBit = gGT->bossID + ADV_REWARD_FIRST_BOSS_KEY;
 
@@ -599,6 +594,7 @@ void AA_EndEvent_DrawMenu(void)
 		gGT->podiumRewardID = STATIC_TROPHY;
 	}
 
+	sdata->Loading.OnBegin.RemBitsConfig8 |= TOKEN_RACE;
 	MainRaceTrack_RequestLoad(levSpawn);
 }
 
