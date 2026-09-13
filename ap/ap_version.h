006f89b22e5dd1bc5ca34cd71008fdb53f819876
dowlle <58628796+dowlle@users.noreply.github.com>
2026-09-13 21:29:29 +0200

    chore: set the build version to v0.2.1-alpha2

--- ap/ap_version.h
+++ ap/ap_version.h
@@ -13,6 +13,6 @@
 
 // Tester-visible build identity. Prerelease labels belong here because the
 // Archipelago world manifest only accepts numeric major.minor.patch versions.
-#define CTR_AP_VERSION "v0.2.1-hit1"
+#define CTR_AP_VERSION "v0.2.1-alpha2"
 
 #endif // AP_VERSION_H
